95f363bebfde2c1ef920d4fc3ed2fdb255956b1a
[taitasu555] <[taitasu08035@icloud.com]>
2021-11-17 20:42:30 +0900

    add:都道府県人口データを取得する非同期処理関数を追加

--- src/components/List/PrefecturesList.tsx
+++ src/components/List/PrefecturesList.tsx
@@ -1,17 +1,40 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { prefecture } from '../../types/prefecture'
 import styles from '../../styles/components/prefectureList.module.css'
+import { APIURL } from '../../common/const'
+import axios from 'axios'
 
 type Props = {
   prefectures: prefecture[]
 }
 
+type Headers = {
+  'X-API-KEY': any
+}
+
+// TODO useMemo, useCallbackが必要なコンポーネント
 export const PrefecturesList: FC<Props> = (props) => {
   const { prefectures } = props
   const [prefectureCode, setPrefectureCode] = useState<string>()
   const getPrefCode: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setPrefectureCode(e.target.value)
   }
+  // prefectureCodeが変更されたら、非同期処理を実行する
+  const headers: Headers = {
+    'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY,
+  }
+  useEffect(() => {
+    const fetcher = async () => {
+      const { data } = await axios.get(
+        `${APIURL}/api/v1/population/composition/perYear??cityCode=-&prefCode=${prefectureCode}`,
+        {
+          headers,
+        }
+      )
+      console.log(data)
+    }
+    fetcher()
+  }, [prefectureCode])
   return (
     <div className={styles.container}>
       {prefectures.map((pref) => {
