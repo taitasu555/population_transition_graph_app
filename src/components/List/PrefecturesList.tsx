da6238071a7d203e0a1f3a31dbff993972ff536c
[taitasu555] <[taitasu08035@icloud.com]>
2021-11-17 20:12:10 +0900

    add: checkboxの作成完了 close #10

--- src/components/List/PrefecturesList.tsx
+++ src/components/List/PrefecturesList.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { prefecture } from '../../types/prefecture'
 import styles from '../../styles/components/prefectureList.module.css'
 
@@ -8,19 +8,28 @@ type Props = {
 
 export const PrefecturesList: FC<Props> = (props) => {
   const { prefectures } = props
-  console.log(prefectures)
+  const [prefectureCode, setPrefectureCode] = useState<string>()
+  const getPrefCode: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPrefectureCode(e.target.value)
+  }
   return (
     <div className={styles.container}>
       {prefectures.map((pref) => {
         return (
           <div className={styles.PrefList} key={pref.prefCode}>
             <label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={getPrefCode}
+                value={pref.prefCode}
+                checked={prefectureCode == pref.prefCode.toString()}
+              />
               {pref.prefName}
             </label>
           </div>
         )
       })}
+      {prefectureCode}
     </div>
   )
 }
